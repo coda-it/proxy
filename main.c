b7eb19de11d19d8310e78deccbc0373ae54b0d81
Oskar Szura <oskar.szura@gmail.com>
2020-02-24 22:33:19 +0100

    CODA-56 - Implement handling parallel requests

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include "utils.h"
 #include "version.h"
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -11,7 +12,7 @@
 #define CONFIG_FILE "./proxy.db"
 #define IP_PORT_SEPARATOR ":"
 #define SERVER_PORT 80
-#define MAX_REQUEST_SIZE 8192
+#define MAX_REQUEST_SIZE 256
 #define MAX_CLIENTS 30
 
 int main(int argc, char const *argv[]) {
@@ -72,95 +73,99 @@ int main(int argc, char const *argv[]) {
   }
 
   i = 0;
+
   struct sockaddr_in serverAddress;
   int serverAddrLen = sizeof(serverAddress);
   int serverFd;
+  startTCPSrv(&serverAddress, &serverFd, SERVER_PORT);
 
-  if ((serverFd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-    perror("server socker");
-    exit(EXIT_FAILURE);
-  }
-
-  serverAddress.sin_family = AF_INET;
-  serverAddress.sin_addr.s_addr = INADDR_ANY;
-  serverAddress.sin_port = htons(SERVER_PORT);
-  memset(serverAddress.sin_zero, '\0', sizeof serverAddress.sin_zero);
-
-  if (bind(serverFd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) <
-      0) {
-    perror("server binding");
-    exit(EXIT_FAILURE);
-  }
-
-  if (listen(serverFd, MAX_CLIENTS) < 0) {
-    perror("server listening");
-    exit(EXIT_FAILURE);
-  }
+  fd_set clientFds;
+  int clientFd;
+  int newClientFd;
+  int maxClientFd = serverFd;
+  FD_ZERO(&clientFds);
+  FD_SET(serverFd, &clientFds);
 
   while (1) {
-    int clientFd;
+    printf("listening for incomming connections\n");
 
-    printf("waiting for client connection\n");
-    if ((clientFd = accept(serverFd, (struct sockaddr *)&serverAddress,
-                           (socklen_t *)&serverAddrLen)) < 0) {
-      perror("client connection accept");
+    if (select(maxClientFd + 1, &clientFds, NULL, NULL, NULL) == -1) {
+      perror("error checking file descriptors");
       exit(EXIT_FAILURE);
     }
 
-    printf("client-server connection established\n");
+    for (clientFd = 0; clientFd <= maxClientFd; clientFd++) {
+      if (FD_ISSET(clientFd, &clientFds)) {
+        if (clientFd == serverFd) {
+          // new connection
+          if ((newClientFd = accept(serverFd, (struct sockaddr *)&serverAddress,
+                                    (socklen_t *)&serverAddrLen)) < 0) {
+            perror("client connection accept");
+            exit(EXIT_FAILURE);
+          }
 
-    struct sockaddr_in targetAddress;
-    int targetFd;
+          FD_SET(newClientFd, &clientFds);
+          if (newClientFd > maxClientFd) {
+            maxClientFd = newClientFd;
+          }
+          printf("client-server connection established\n");
 
-    targetAddress.sin_family = AF_INET;
-    targetAddress.sin_addr.s_addr = inet_addr(targetIP);
-    targetAddress.sin_port = htons(atoi(targetPort));
-    memset(targetAddress.sin_zero, '\0', sizeof targetAddress.sin_zero);
+        } else {
+          struct sockaddr_in targetAddress;
+          int targetFd;
 
-    if ((targetFd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-      perror("target socket");
-      exit(EXIT_FAILURE);
-    }
+          targetAddress.sin_family = AF_INET;
+          targetAddress.sin_addr.s_addr = inet_addr(targetIP);
+          targetAddress.sin_port = htons(atoi(targetPort));
+          memset(targetAddress.sin_zero, '\0', sizeof targetAddress.sin_zero);
 
-    if (connect(targetFd, (struct sockaddr *)&targetAddress,
-                sizeof(targetAddress)) < 0) {
-      perror("target connection");
-      exit(EXIT_FAILURE);
-    }
+          if ((targetFd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+            perror("target socket");
+            exit(EXIT_FAILURE);
+          }
 
-    printf("sending request from client to target\n");
-    char request[MAX_REQUEST_SIZE];
-    memset(request, '\0', sizeof request);
+          if (connect(targetFd, (struct sockaddr *)&targetAddress,
+                      sizeof(targetAddress)) < 0) {
+            perror("target connection");
+            exit(EXIT_FAILURE);
+          }
 
-    while ((n = read(clientFd, request, MAX_REQUEST_SIZE)) > 0) {
-      if (write(targetFd, request, n) < 0) {
-        printf("writing content from client to target failed - probably "
-               "SIGPIPE\n");
+          printf("sending request from client to target\n");
+          char request[MAX_REQUEST_SIZE];
+          memset(request, '\0', sizeof request);
+
+          while ((n = read(clientFd, request, sizeof(request))) > 0) {
+            if (write(targetFd, request, n) < 0) {
+              perror("writing content from client to target failed - probably "
+                     "SIGPIPE\n");
+            }
+            if (n < MAX_REQUEST_SIZE) {
+              break;
+            }
+          }
+
+          printf("sending response from target to client\n");
+          char response[MAX_REQUEST_SIZE];
+          memset(response, '\0', sizeof response);
+
+          while ((n = read(targetFd, response, sizeof(response))) > 0) {
+            printf("%s", response);
+            if (write(clientFd, response, n) < 0) {
+              perror("writing content from target to client failed - probably "
+                     "SIGPIPE\n");
+            }
+            if (n < MAX_REQUEST_SIZE) {
+              break;
+            }
+          }
+
+          printf("request completed\n");
+          FD_CLR(clientFd, &clientFds);
+          close(clientFd);
+          close(targetFd);
+        }
       }
-
-      if (n < MAX_REQUEST_SIZE) {
-        break;
-      }
     }
-
-    printf("sending response from target to client\n");
-    char response[MAX_REQUEST_SIZE];
-    memset(response, '\0', sizeof response);
-
-    while ((n = read(targetFd, response, MAX_REQUEST_SIZE)) > 0) {
-      if (write(clientFd, response, n) < 0) {
-        printf("writing content from target to client failed - probably "
-               "SIGPIPE\n");
-      }
-
-      if (n < MAX_REQUEST_SIZE) {
-        break;
-      }
-    }
-
-    printf("request completed\n");
-    close(clientFd);
-    close(targetFd);
   }
 
   printf("closing proxy\n");
